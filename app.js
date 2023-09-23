21dee95f4bf904f6693b22b22c8168bd1ef33338
Ido <Idosabach11@gmail.com>
2023-09-24 02:32:19 +0300

    learn js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-function addname(){
+function add_name(){
   var newname = document.getElementById("box");
   var newel = document.createElement("div");
   newel.innerHTML = newname.value;
-  document.getElementById("fuckblok").appendChild(newel);
+  document.getElementById("fuck_blok").appendChild(newel);
 }
 
+
+var add = document.createElement("div");
+add.innerHTML = "hi im ido";
+add.style.color = "red";
+var par = document.getElementById("fuck_blok");
+par.appendChild(add);
 
